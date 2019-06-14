d16f8a230bc2ce06fd57d161cf64d890b3b35fdc
Chuck <Chuck@chuck-home>
2019-06-14 13:25:25 -0700

    First commit uploading all of the files

---
+++ Calc/src/CalcDemo/CalcMain.java
@@ -0,0 +1,15 @@
+package CalcDemo;
+
+public class CalcMain {
+
+	public static void main(String[] args) {
+		
+		int add = Calculator.add(3,5);
+		int sub = Calculator.sub(5,3);
+		
+		System.out.println("3 + 5 = " + add);
+		System.out.println("5 + 3 = " + sub);
+
+	}
+
+}
---
+++ Calc/src/CalcDemo/Calculator.java
@@ -0,0 +1,14 @@
+package CalcDemo;
+
+public class Calculator {
+	
+	public static int add(int num1,int num2){
+		int r = num1 + num2;
+		return r;
+	}
+	public static int sub(int num1,int num2){
+		int r = num1 - num2;
+		return r;
+	}
+
+}
